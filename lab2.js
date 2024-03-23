e954070e29c22645370cb742db4b5e8497bff736
maddiemaclean <156142897+maddiemaclean@users.noreply.github.com>
2024-03-23 15:08:12 -0300

    Create lab2.js

---
+++ lab2.js
@@ -0,0 +1,60 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function askUser() {
+    rl.question("Select a command: ", function(userInput) {
+        if (userInput === "q") {
+            rl.close();
+        } else if (userInput === "n") {
+            rl.question("Insert a number: ", function(num) {
+                const factNum = is_factorial(num);
+                console.log(factNum);
+
+                if (factNum === 0) {
+                    console.log(num + " is not the factor of another number");
+                } else if (factNum < 0) {
+                    console.log(num + " is a negative number");
+                } else {
+                    console.log(num + " is the factorial number of: " + factNum);
+                }
+
+                askUser(); 
+            });
+        } else {
+            console.log("Invalid command. Please enter 'n' or 'q'.");
+            askUser();
+        }
+    });
+}
+
+console.log("   n: Check a value");
+console.log("   q: quit");
+
+askUser(); // Initial call to start the process
+
+function is_factorial(numIn) {
+    const num = parseInt(numIn);
+
+    if (num < 0) {
+        return -1;
+    } else if (num === 0) {
+        return 0;
+    } else {
+        let i = 1;
+        let result = 1;
+
+        while (result < num) {
+            i++;
+            result *= i;
+        }
+
+        if (result === num) {
+            return i;
+        } else {
+            return 0;
+        }
+    }
+}
