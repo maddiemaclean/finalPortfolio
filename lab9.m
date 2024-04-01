5d913aa11c0573efae0156f51d7a0838613b0f5c
maddiemaclean <156142897+maddiemaclean@users.noreply.github.com>
2024-04-01 13:40:45 -0300

    Update lab9.m

--- lab9.m
+++ lab9.m
@@ -1,10 +1,10 @@
 disp("")
 function retval = countLetters( stringIn )
 vector = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-function retval = countLetters(input) # retval is how we return values in octave, at the end, retval will be returned.
+function retval = countLetters(input) % retval is how we return values in octave, at the end, retval will be returned.
 for i = 1:length (stringIn) # example of a for loop in octave
 	currString = toupper(stringIn); 
-	if(double(currString(i)) > 64 & double(currString(i)) < 91) # in octave () brackets are used rather than [] for arrays
+	if(double(currString(i)) > 64 & double(currString(i)) < 91) % in octave () brackets are used rather than [] for arrays
 		if (vector (double(currString(i))-64 != 0)) 
 			vector(double(currString(i))-64)++;
 		else
