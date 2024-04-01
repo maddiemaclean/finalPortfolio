6f15e0d7def7ec235d1a924d8c39eedce4789186
maddiemaclean <156142897+maddiemaclean@users.noreply.github.com>
2024-04-01 13:25:17 -0300

    Update lab7.js

--- lab7.js
+++ lab7.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs'); //import for FileIO
 let abbyTotal = 0;
 let tysonTotal = 0;
 let zackTotal = 0;
@@ -6,12 +6,12 @@ const files = ['calculations1.json', 'calculations2.json', 'calculations3.json',
 
 function readFiles(filesIn) {
     for (let i = 0; i < filesIn.length; i++) {
-        fs.readFile(filesIn[i], 'utf8', (err, data) => {
+        fs.readFile(filesIn[i], 'utf8', (err, data) => { //one way to do fileIO in JavaScript
             if (err) {
                 console.error(err);
                 return;
             }
-            const jsonData = JSON.parse(data);
+            const jsonData = JSON.parse(data); //how to parse JSON
             const calculations1 = jsonData.data.calculations;
             calc(calculations1);
 
@@ -24,7 +24,7 @@ function readFiles(filesIn) {
 
 function calc(calculationsIn) {
     let correct = 0;
-    calculationsIn.forEach(j => {
+    calculationsIn.forEach(j => { //example of a for each loop in JS
         if (j.performer === 'TRUE') {
             correct = j.calc;
         }
@@ -42,8 +42,8 @@ function calc(calculationsIn) {
 
 function printTotals() {
     let arr = [abbyTotal, tysonTotal, zackTotal];
-    arr.sort(function(a, b){return a - b});
-    for(let i=0; i<arr.length;i++){
+    arr.sort(function(a, b){return a - b}); // sort(): can be used to sort an array in ascending and descending order
+    for(let i=0; i<arr.length;i++){ //example of a for-loop in JS
         if(abbyTotal == arr[i]){
             console.log(i+1 + ". Abby (" + arr[i]+")");
         }
