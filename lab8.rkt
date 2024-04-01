6e5022b04b81a5345b757deaa81f55870cf34b07
maddiemaclean <156142897+maddiemaclean@users.noreply.github.com>
2024-04-01 13:34:22 -0300

    Update lab8.rkt

--- lab8.rkt
+++ lab8.rkt
@@ -2,10 +2,10 @@
 
 (define(palindrome wordIn)
   (cond
-   [(= (string-length wordIn) 0)#t]
+   [(= (string-length wordIn) 0)#t] ; string-length: can be used to find the length of a string
    [(= (string-length wordIn) 1)#t]
-    [(equal?(string-ref wordIn 0)(string-ref wordIn(-(string-length wordIn)1)))#t]
-     (palindrome(substring wordIn 1(- (string-length wordIn)1)))
+    [(equal?(string-ref wordIn 0)(string-ref wordIn(-(string-length wordIn)1)))#t] ;string-ref: returns the character in position k. k must be smaller than the length
+     (palindrome(substring wordIn 1(- (string-length wordIn)1))) ; substring: a basic substring method that returns a new string when given two positions
      [else
      #f]))
 
@@ -16,7 +16,7 @@
 (define (casear wordIn keyVal pos)
   (cond
   [(< pos (string-length wordIn))
-  (cons(integer->char (+ keyVal (char->integer (string-ref wordIn pos))))
+  (cons(integer->char (+ keyVal (char->integer (string-ref wordIn pos)))) ; the -> is how to convert data types, ie a interger to a character.
   (casear wordIn keyVal (add1 pos)))]))
 
 (define word2 "abcde")
